0ec0768fd10a78f48131fd4fe8b8a205b8c98183
natechaufan88 <natechaufan88@gmail.com>
2015-08-18 09:21:40 -0700

    Hello world demonstration
    This shit
    won't commit.

---
+++ HelloWorld2/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * This program will output a string of text. My first Java program!
+ * 
+ * Nathan Chau
+ * August 18, 2015
+ */
+public class HelloWorld 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Nathan."); //Outputs the string in quotations
+    }
+}
