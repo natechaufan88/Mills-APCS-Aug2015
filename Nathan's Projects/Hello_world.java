d690c6cab3277167ec10b54c6ca6af58d6f61f34
natechaufan88 <natechaufan88@gmail.com>
2015-08-17 21:31:50 -0700

    Hello world demonstration
    I'm excited to have written my first program on BlueJ!

---
+++ Nathan's Projects/Hello_world.java
@@ -0,0 +1,11 @@
+/**
+ * This is Nathan's first program.
+ * 
+ * Nathan Chau
+ * August 17, 2015
+ */
+public class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World! From Nathan Chau.");
+    }
+}
