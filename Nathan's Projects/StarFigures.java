c553e1303179fa17df801a3bbffe866f403ea02f
natechaufan88 <natechaufan88@gmail.com>
2015-08-25 09:02:50 -0700

    Printing Asterisks
    We won banana bread!

---
+++ Nathan's Projects/StarFigures.java
@@ -0,0 +1,39 @@
+
+public class StarFigures {
+    public static void main(String[] args) {
+        star5();
+        star5();
+        star2();
+        star1();
+        star2();
+        System.out.println();
+        star5();
+        star5();
+        star2();
+        star1();
+        star2();
+        star5();
+        star5();
+        System.out.println();
+        star1();
+        star1();
+        star1();
+        star5();
+        star5();
+        star2();
+        star1();
+        star2();
+    }
+    
+    public static void star5() {
+        System.out.println("*****");
+    }
+    
+    public static void star2() {
+        System.out.println(" * *");
+    }
+    
+    public static void star1() {
+        System.out.println("  *");
+    }
+}
