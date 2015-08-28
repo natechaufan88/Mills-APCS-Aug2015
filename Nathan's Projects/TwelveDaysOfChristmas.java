4b362227c508f43c5245e56bcf68d082c0a11ed2
natechaufan88 <natechaufan88@gmail.com>
2015-08-27 20:47:46 -0700

    TwelveDaysOfChristmas

---
+++ Nathan's Projects/TwelveDaysOfChristmas.java
@@ -0,0 +1,156 @@
+/* Nathan Chau
+ * HW Day 10
+ * August 27. 2015
+ */
+
+public class TwelveDaysOfChristmas {
+    public static void main(String[] args) {
+        first();
+        gift1();
+        second();
+        gift2();
+        third();
+        gift3();
+        fourth();
+        gift4();
+        fifth();
+        gift5();
+        sixth();
+        gift6();
+        seventh();
+        gift7();
+        eighth();
+        gift8();
+        ninth();
+        gift9();
+        tenth();
+        gift10();
+        eleventh();
+        gift11();
+        twelfth();
+        gift12();
+    }
+    
+    public static void trueLove() {
+        System.out.println("my true love sent to me");
+    }
+    
+    public static void first() {
+        System.out.println("On the first day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift1() {
+        System.out.println("a partridge in a pear tree.\n");
+    }
+    
+    public static void second() {
+        System.out.println("On the second day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift2() {
+        System.out.println("two turtles doves, and");
+        gift1();
+    }
+    
+    public static void third() {
+        System.out.println("On the third day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift3() {
+        System.out.println("three French hens,");
+        gift2();
+    }
+    
+    public static void fourth() {
+        System.out.println("On the fourth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift4() {
+        System.out.println("four calling birds,");
+        gift3();
+    }
+    
+    public static void fifth() {
+        System.out.println("On the fifth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift5() {
+        System.out.println("five golden rings,");
+        gift4();
+    }
+    
+    public static void sixth() {
+        System.out.println("On the sixth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift6() {
+        System.out.println("six geese a-laying,");
+        gift5();
+    }
+    
+    public static void seventh() {
+        System.out.println("On the seventh day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift7() {
+        System.out.println("seven swans a-swimming,");
+        gift6();
+    }
+    
+    public static void eighth() {
+        System.out.println("On the eighth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift8() {
+        System.out.println("eight maids a-milking,");
+        gift7();
+    }
+    
+    public static void ninth() {
+        System.out.println("On the ninth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift9() {
+        System.out.println("nine ladies dancing,");
+        gift8();
+    }
+    
+    public static void tenth() {
+        System.out.println("On the tenth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift10() {
+        System.out.println("ten lords a-leaping,");
+        gift9();
+    }
+    
+    public static void eleventh() {
+        System.out.println("On the eleventh day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift11() {
+        System.out.println("eleven pipers piping,");
+        gift10();
+    }
+    
+    public static void twelfth() {
+        System.out.println("On the twelfth day of Christmas,");
+        trueLove();
+    }
+    
+    public static void gift12() {
+        System.out.println("twelve drummers drumming,");
+        gift11();
+    }
+}
